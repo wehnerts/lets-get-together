d3ec11df03bdafbcae70eea0b3b2547c81d6f09e
wehnerts <wehnerts@wi-wehnert.de>
2022-05-23 15:55:09 +0200

    Added Backend Functionality and Tests for adding a new ActionItem

--- backend/src/main/java/de/wehnerts/backend/service/ActionItemService.java
+++ backend/src/main/java/de/wehnerts/backend/service/ActionItemService.java
@@ -1,5 +1,6 @@
 package de.wehnerts.backend.service;
 
+import de.wehnerts.backend.dto.NewActionItemDto;
 import de.wehnerts.backend.model.ActionItem;
 import de.wehnerts.backend.repository.ActionItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +16,31 @@ public class ActionItemService {
     public ActionItemService(ActionItemRepo actionItemRepo) {
         this.actionItemRepo = actionItemRepo;
     }
+
+
+
     public List<ActionItem> getActionItems(){return actionItemRepo.findAll();
     }
 
     public ActionItem getActionItemById(String id) {
         return actionItemRepo.findById(id)
                 .orElseThrow(()->new NoSuchElementException("Action item with "+ id +" not found."));
+    }
+    public ActionItem addNewActionItem(NewActionItemDto newActionItemDto) {
+        ActionItem newActionItem = new ActionItem();
+        if(newActionItemDto.getActionTitle()==null){
+            throw new IllegalArgumentException("Action Title mustn't be NULL");
+        }
+        newActionItem.setActionTitle(newActionItemDto.getActionTitle());
+        newActionItem.setImageName(newActionItemDto.getImageName());
+        newActionItem.setActionDescription(newActionItemDto.getActionDescription());
+        newActionItem.setChildFriendly(newActionItemDto.getChildFriendly());
+        newActionItem.setOpeningSeason(newActionItemDto.getOpeningSeason());
+        newActionItem.setOpeningHours(newActionItemDto.getOpeningHours());
+        newActionItem.setEstDuration(newActionItemDto.getEstDuration());
+        newActionItem.setPrice(newActionItemDto.getPrice());
+        newActionItem.setHomepage(newActionItemDto.getHomepage());
+
+        return actionItemRepo.insert(newActionItem);
     }
 }
