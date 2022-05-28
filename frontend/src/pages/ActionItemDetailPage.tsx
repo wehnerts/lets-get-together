546344d9fc47f1551cdecfe71f42b21a42a4e660
wehnerts <wehnerts@wi-wehnert.de>
2022-05-28 12:07:22 +0200

    Added delete ActionItem and some css styling

--- frontend/src/pages/ActionItemDetailPage.tsx
+++ frontend/src/pages/ActionItemDetailPage.tsx
@@ -1,13 +1,29 @@
-import {useNavigate} from "react-router";
-import React from "react";
+import {useNavigate, useParams} from "react-router";
+import React, {useEffect} from "react";
 import "./ActionItemDetailPage.css"
 import ActionItemDisplayDetails from "../components/ActionItemDisplayDetails";
+import useActionItems from "../hooks/useActionItems";
+import useDetailedActionItem from "../hooks/useDetailedActionItem";
 
 export default function ActionItemDetailPage(){
-   const navigate = useNavigate()
+    const {id} = useParams()
+    const {detailedActionItem, getActionItemById} = useDetailedActionItem()
+    const navigate = useNavigate()
+    const {deleteActionItem}=useActionItems()
+
+    useEffect(() => {
+        if (id) {
+            getActionItemById(id)
+        }
+        // eslint-disable-next-line
+    }, [id])
+
    return(
-       <div> Was geht digger?
-       <ActionItemDisplayDetails/>
+       <div>
+           {detailedActionItem&& <div>
+           <ActionItemDisplayDetails detailedActionItem={detailedActionItem}/>
+           <button onClick={() => deleteActionItem(detailedActionItem.id)}> ❌ </button>
+           </div>}
            <button onClick={() => navigate(-1)}>Back</button>
        </div>
    )
--- frontend/src/pages/ActionItemsPage.tsx
+++ frontend/src/pages/ActionItemsPage.tsx
@@ -2,18 +2,19 @@ import {useNavigate} from "react-router"
 import ActionItemsGallery from "../components/ActionItemsGallery";
 import React from "react";
 import {ActionItem} from "../model/ActionItem";
+import './ActionItemsPage.css'
 
 type NewActionItemProps={
     actionItems: ActionItem[]
 }
 
 const ActionItemsPage=({actionItems}:NewActionItemProps)=>{
-
     const navigate = useNavigate()
-
     return(
         <div>
+            <div className={"gallery"}>
             <ActionItemsGallery actionItems={actionItems}/>
+            </div>
             <button onClick={()=>navigate(-1)}>Back</button>
             <button onClick={() => navigate("/new-action")}>New Action</button>
         </div>
