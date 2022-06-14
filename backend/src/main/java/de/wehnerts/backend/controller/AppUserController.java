6356a03ff39b53ce4dc546215dbae75abae01464
Sönke Wehnert <76809318+wehnerts@users.noreply.github.com>
2022-06-14 14:56:54 +0200

    Update backend/src/main/java/de/wehnerts/backend/controller/AppUserController.java
    Co-authored-by: Gadenko <102051504+Gadenko@users.noreply.github.com>

--- backend/src/main/java/de/wehnerts/backend/controller/AppUserController.java
+++ backend/src/main/java/de/wehnerts/backend/controller/AppUserController.java
@@ -4,7 +4,6 @@ import de.wehnerts.backend.dto.AppUserDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.security.Principal;
 
 @RestController
