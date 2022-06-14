2026564cd406135318414b5082ae00f76a000f70
Sönke Wehnert <76809318+wehnerts@users.noreply.github.com>
2022-06-14 14:57:36 +0200

    Update backend/src/main/java/de/wehnerts/backend/security/repository/AppUserRepository.java
    Co-authored-by: Gadenko <102051504+Gadenko@users.noreply.github.com>

--- backend/src/main/java/de/wehnerts/backend/security/repository/AppUserRepository.java
+++ backend/src/main/java/de/wehnerts/backend/security/repository/AppUserRepository.java
@@ -3,7 +3,6 @@ package de.wehnerts.backend.security.repository;
 import de.wehnerts.backend.security.model.AppUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
