ada17288ee9f8f383e23ee24de51c84e8f6d879a
Sönke Wehnert <76809318+wehnerts@users.noreply.github.com>
2022-06-14 14:56:10 +0200

    Update backend/src/main/java/de/wehnerts/backend/security/service/JWTUtilService.java
    Co-authored-by: Gadenko <102051504+Gadenko@users.noreply.github.com>

--- backend/src/main/java/de/wehnerts/backend/security/service/JWTUtilService.java
+++ backend/src/main/java/de/wehnerts/backend/security/service/JWTUtilService.java
@@ -4,7 +4,6 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
